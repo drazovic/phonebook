1e9b7fa29cd45425971453e7e745befda216687d
drazovic <idrazovic@gmail.com>
2021-01-18 13:58:33 +0100

    users controller added

--- api/controllers/users.controller.go
+++ api/controllers/users.controller.go
@@ -1,16 +1,35 @@
 package controllers
 
 import (
+	"encoding/json"
 	"net/http"
 
+	u "api/utils"
+	"api/models"
 )
 
 // Signup ...
 func Signup(w http.ResponseWriter, r *http.Request) {
+	user := &models.User{}
+	err := json.NewDecoder(r.Body).Decode(user) //decode the request body into struct and failed if any error occur
+	if err != nil {
+		u.Respond(w, u.Message(false, "Invalid request"))
+		return
+	}
 
+	resp := user.Create() //Create account
+	u.Respond(w, resp)
 }
 
 // Login ...
 func Login(w http.ResponseWriter, r *http.Request) {
+	user := &models.User{}
+	err := json.NewDecoder(r.Body).Decode(user) //decode the request body into struct and failed if any error occur
+	if err != nil {
+		u.Respond(w, u.Message(false, "Invalid request"))
+		return
+	}
 
+	resp := models.Login(user.Email, user.Password)
+	u.Respond(w, resp)
 }
