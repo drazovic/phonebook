a4135f7739c4fd19e281fd1f6a7263ae67601471
drazovic <idrazovic@gmail.com>
2021-01-18 14:37:09 +0100

    signin navigation fix

--- web-app/src/app/auth/auth.component.ts
+++ web-app/src/app/auth/auth.component.ts
@@ -44,7 +44,7 @@ export class AuthComponent implements OnInit {
         authObservable.subscribe(
             (responseData) => {
                 this.isLoading = false;
-                this.router.navigate(['/recipes']);
+                this.router.navigate(['/contacts']);
             },
             (errorMessage) => {
                 this.error = errorMessage;
