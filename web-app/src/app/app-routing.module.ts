33a093ef7809f2555b26279b0b624ae15b5b3e51
drazovic <idrazovic@gmail.com>
2021-01-18 14:03:12 +0100

    routes added

--- web-app/src/app/app-routing.module.ts
+++ web-app/src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
+import { ContactsComponent } from './contacts/contacts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/contacts', pathMatch: 'full' },
+  {
+    path: 'auth',
+    component: AuthComponent,
+  },
+  {
+    path: 'contacts',
+    component: ContactsComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
