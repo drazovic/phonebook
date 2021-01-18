830f74fb50327e1602609eb8c8c133148a4c49f1
drazovic <idrazovic@gmail.com>
2021-01-18 13:57:34 +0100

    cors added

--- api/main.go
+++ api/main.go
@@ -4,13 +4,20 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/rs/cors"
+
+	"api/handlers"
 	"api/router"
 )
 
 func main() {
 	router := router.GetRouter()
 
-	err := http.ListenAndServe(":8080", router)
+	router.Use(handlers.JwtAuthentication) //attach JWT auth middleware
+
+	handler := cors.Default().Handler(router)
+
+	err := http.ListenAndServe(":8080", handler)
 	if err != nil {
 		fmt.Print(err)
 	}
