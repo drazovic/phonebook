10297c2954a66119daa6255ad2d4c6a641368c7f
drazovic <idrazovic@gmail.com>
2021-01-19 10:26:45 +0100

    auth guard implemented

--- web-app/src/app/app-routing.module.ts
+++ web-app/src/app/app-routing.module.ts
@@ -1,22 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
+import { AuthGard } from './auth/auth.gard';
 import { ContactsComponent } from './contacts/contacts.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/contacts', pathMatch: 'full' },
-  {
-    path: 'auth',
-    component: AuthComponent,
-  },
-  {
-    path: 'contacts',
-    component: ContactsComponent,
-  },
+    { path: '', redirectTo: '/contacts', pathMatch: 'full' },
+    {
+        path: 'auth',
+        component: AuthComponent,
+    },
+    {
+        path: 'contacts',
+        component: ContactsComponent,
+        canActivate: [AuthGard],
+    },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
 export class AppRoutingModule {}
