9ca25b9f543ca98406b3103f8c7252d8825640bc
drazovic <idrazovic@gmail.com>
2021-01-20 12:00:18 +0100

    code format updated

--- web-app/src/app/app-routing.module.ts
+++ web-app/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { AuthComponent } from './auth/auth.component';
 import { AuthGard } from './auth/auth.gard';
 import { ContactsComponent } from './contacts/contacts.component';
