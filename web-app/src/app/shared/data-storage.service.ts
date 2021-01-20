d1a046301c3a5d80b30e074680fd6a092645b8fa
drazovic <idrazovic@gmail.com>
2021-01-20 12:01:52 +0100

    data storage updated

--- web-app/src/app/shared/data-storage.service.ts
+++ web-app/src/app/shared/data-storage.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map, tap } from 'rxjs/operators';
 
 import { ContactsService } from '../contacts/contacts.service';
 import { environment } from 'src/environments/environment';
-import { Contact } from '../contacts/contact.model';
+
+import { ApiResponse } from '../api-response.model';
+import { Contact } from '../contacts/interfaces';
+import { map, tap } from 'rxjs/operators';
 
 @Injectable({
     providedIn: 'root',
@@ -17,33 +19,53 @@ export class DataStorageService {
 
     storeNewContact(contact: Contact) {
         this.http
-            .post(`${environment.apiUrl}/contacts`, contact)
-            .subscribe((contact) => {
-                console.log(contact);
+            .post<ApiResponse<Contact>>(
+                `${environment.apiUrl}/contacts`,
+                contact
+            )
+            .subscribe((response) => {
+                console.log(response);
+
+                if (!response.data.ID) {
+                    return;
+                }
+                this.contactsService.addContact(response.data);
             });
     }
 
     updateContact(contact: Contact) {
         this.http
-            .patch(`${environment.apiUrl}/contacts/${contact.id}`, contact)
-            .subscribe((contact) => {
-                console.log(contact);
+            .patch<ApiResponse<Contact>>(
+                `${environment.apiUrl}/contacts/${contact.ID}`,
+                contact
+            )
+            .subscribe((response) => {
+                if (!response.data.ID) {
+                    return;
+                }
+                this.contactsService.updateContact(
+                    response.data.ID,
+                    response.data
+                );
             });
     }
 
     deleteContact(contactId: number) {
         this.http
-            .delete(`${environment.apiUrl}/contacts/${contactId}`)
+            .delete<ApiResponse<Contact>>(
+                `${environment.apiUrl}/contacts/${contactId}`
+            )
             .subscribe((contact) => {
-                console.log(contact);
+                this.contactsService.deleteContacts(contactId);
             });
     }
 
     fetchContacts() {
         return this.http
-            .get<Contact[]>(`${environment.apiUrl}/contacts`)
-            .subscribe((contacts) => {
-                this.contactsService.setContacts(contacts);
-            });
+            .get<ApiResponse<Contact[]>>(`${environment.apiUrl}/contacts`)
+            .pipe(map((response) => response.data))
+            .subscribe((contacts) =>
+                this.contactsService.setContacts(contacts)
+            );
     }
 }
