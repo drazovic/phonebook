0eef5e80f00bb5fd6f83b01284958cdbd2873e0d
drazovic <idrazovic@gmail.com>
2021-01-20 11:57:14 +0100

    corsHandler updated

--- api/main.go
+++ api/main.go
@@ -15,9 +15,14 @@ func main() {
 
 	router.Use(handlers.JwtAuthentication) //attach JWT auth middleware
 
-	handler := cors.Default().Handler(router)
+	corsHandler := cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:4200"},
+		AllowCredentials: true,
+		AllowedMethods:   []string{"GET", "POST", "PATCH", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Authorization", "Content-Type"},
+	}).Handler(router)
 
-	err := http.ListenAndServe(":8080", handler)
+	err := http.ListenAndServe(":8080", corsHandler)
 	if err != nil {
 		fmt.Print(err)
 	}
